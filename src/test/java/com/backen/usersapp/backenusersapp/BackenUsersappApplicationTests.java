fec4722d6f0bd08c9d2d01390873349925aa62ec
Luis Carlos <luiscarjo157@gmail.com>
2023-12-09 18:01:37 -0500

    subien proyecto backend login

---
+++ src/test/java/com/backen/usersapp/backenusersapp/BackenUsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backen.usersapp.backenusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenUsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
