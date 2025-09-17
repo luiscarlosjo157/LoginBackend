4ec8e4e75e3d6db51c8a7f267cf4b2bcb8b6e420
Luis Carlos <luiscarjo157@gmail.com>
2025-09-17 09:58:02 -0500

    se configura la cacle test para que el CI detecte la clase principal2

--- src/test/java/com/backen/usersapp/backenusersapp/BackenUsersappApplicationTests.java
+++ src/test/java/com/backen/usersapp/backenusersapp/BackenUsersappApplicationTests.java
@@ -3,8 +3,11 @@ package com.backen.usersapp.backenusersapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BackenUsersappApplicationTests {
+import com.backend.usersapp.backenusersapp.BackendUsersappApplication;
+
+@SpringBootTest(classes = BackendUsersappApplication.class) 
+class BackenUsersappApplicationTests{
+
 
 	@Test
 	void contextLoads() {
